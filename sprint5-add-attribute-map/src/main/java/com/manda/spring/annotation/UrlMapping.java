f186657acdde0d16034c273598d94a80fe0e2fd8
Manda3312 <ikotokelymanda@gmail.com>
2025-11-30 12:58:06 -0800

    Sprint6Ter-get-brace-values

--- sprint5-add-attribute-map/src/main/java/com/manda/spring/annotation/UrlMapping.java
+++
@@ -1,9 +0,0 @@
-package com.manda.spring.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface UrlMapping {
-    String path() default "";
-}
